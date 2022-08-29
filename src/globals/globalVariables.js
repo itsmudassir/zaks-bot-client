7589da4932b22784937c1f0fab396d068a008628
optix <itsmudassir>
2022-08-29 17:22:10 +0500

    3nd

--- src/globals/globalVariables.js
+++ src/globals/globalVariables.js
@@ -1,4 +1,4 @@
 // const apiUrl = "http://localhost:7777";
-const apiUrl = "https://zaks-bot-server-5coylqdhu-booking-bot.vercel.app";
+const apiUrl = "https://zaks-bot-server.vercel.app";
 
 export {apiUrl};
